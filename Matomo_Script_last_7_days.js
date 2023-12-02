5af08a3d2ab6500bdb900f2104842ba19dd1f5e5
Tony Brüser <tonybrueser.1995@gmail.com>
2023-12-02 14:37:43 +0100

    added real time views for medium sized widget

--- Matomo_Script_last_7_days.js
+++ Matomo_Script_last_7_days.js
@@ -1,108 +1,176 @@
-// Read the parameter from the widget parameter
-const widgetParameter = args.widgetParameter;
-
-// Parse the parameter to get the Site-ID
-// Read the `siteId` from the widget parameter
-const siteId = args.widgetParameter || "1"; // Set a default Site-ID if none was provided
-
-// Constants for the Matomo API
-const matomoUrl = "https://analytics.example.com";
-const tokenAuth = "your-auth-token";
+// Konstanten für die Matomo-API
+const matomoUrl = "MATOMO_URL"; // change it to the Matomo URL (analytic.example.com)
+const tokenAuth = "APY_KEY"; // change it -> API KEY (https://matomo.org/faq/general/faq_114/)
+const siteId = args.widgetParameter || "1"; // Standard-Site-ID, can bet set as parameter
 
 
-// Create widget
-const widget = new ListWidget();
-
-// Set the widget's background color
-widget.backgroundColor = new Color("#000000");
-
-// API requests for the current and previous 7 days
-const currentDate = new Date();
-const sevenDaysAgo = new Date();
-sevenDaysAgo.setDate(currentDate.getDate() - 7);
-const fourteenDaysAgo = new Date();
-fourteenDaysAgo.setDate(currentDate.getDate() - 14);
-
-// Functions to format the dates for the API request
+// Functions for formatting the data for the API request
 function formatDate(date) {
-  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 }
 
 async function fetchMatomoData(date1, date2) {
-  const formattedDate1 = formatDate(date1);
-  const formattedDate2 = formatDate(date2);
-  const url = `${matomoUrl}?module=API&method=VisitsSummary.getVisits&idSite=${siteId}&period=range&date=${formattedDate1},${formattedDate2}&format=JSON&token_auth=${tokenAuth}`;
-  const response = await new Request(url).loadJSON();
-  return response.value;
+    const formattedDate1 = formatDate(date1);
+    const formattedDate2 = formatDate(date2);
+    const url = `${matomoUrl}?module=API&method=VisitsSummary.getVisits&idSite=${siteId}&period=range&date=${formattedDate1},${formattedDate2}&format=JSON&token_auth=${tokenAuth}`;
+    const request = new Request(url);
+    const response = await request.loadJSON();
+    return response.value;
 }
 
 async function fetchMatomoDataViews(date1, date2) {
-  const formattedDate1 = formatDate(date1);
-  const formattedDate2 = formatDate(date2);
-  const url = `${matomoUrl}?module=API&method=Actions.getPageUrls&idSite=${siteId}&period=range&date=${formattedDate1},${formattedDate2}&format=JSON&token_auth=${tokenAuth}`;
-  
+    const formattedDate1 = formatDate(date1);
+    const formattedDate2 = formatDate(date2);
+    const url = `${matomoUrl}?module=API&method=Actions.getPageUrls&idSite=${siteId}&period=range&date=${formattedDate1},${formattedDate2}&format=JSON&token_auth=${tokenAuth}`;
+    const request = new Request(url);
+    try {
+        const response = await request.loadJSON();
+        let totalViews = 0;
+        response.forEach(page => {
+            totalViews += page.nb_hits;
+        });
+        return totalViews;
+    } catch (error) {
+        console.error(`Error fetching Matomo data views: ${error}`);
+        return 0; // Return a default/fallback value
+    }
+}
+
+// Function to fetch real-time visitor data
+async function fetchRealTimeVisitors() {
+  const url = `${matomoUrl}?module=API&method=Live.getLastVisitsDetails&idSite=${siteId}&period=day&date=today&format=JSON&token_auth=${tokenAuth}`;
   try {
     const response = await new Request(url).loadJSON();
-
-    // Calculate the total number of page views
-    let totalViews = 0;
-    response.forEach(page => {
-      totalViews += page.nb_hits;
-    });
-
-    return totalViews;
+    return response.slice(0, 10); // Take the last 10 visits
   } catch (error) {
-    return null; // or set a default value
+    console.error(`Error fetching real-time visitors: ${error}`);
+    return []; // Empty if an error occurs
   }
 }
 
+// Hauptfunktion für das Abrufen und Berechnen der Daten
+async function fetchDataAndCalculate() {
+    const currentDate = new Date();
+    const sevenDaysAgo = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 7);
+    const fourteenDaysAgo = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 14);
 
-// Fetch data
-const currentData = await fetchMatomoData(sevenDaysAgo, currentDate);
-const previousData = await fetchMatomoData(fourteenDaysAgo, sevenDaysAgo);
+    const currentData = await fetchMatomoData(sevenDaysAgo, currentDate);
+    const previousData = await fetchMatomoData(fourteenDaysAgo, sevenDaysAgo);
+    const currentDataViews = await fetchMatomoDataViews(sevenDaysAgo, currentDate);
+    const previousDataViews = await fetchMatomoDataViews(fourteenDaysAgo, sevenDaysAgo);
+    
+    const realTimeVisitors = await fetchRealTimeVisitors();
 
-const currentDataViews = await fetchMatomoDataViews(sevenDaysAgo, currentDate);
-const previousDataViews = await fetchMatomoDataViews(fourteenDaysAgo, sevenDaysAgo);
+    const percentChange = ((currentData - previousData) / previousData) * 100;
+    const percentChangeViews = ((currentDataViews - previousDataViews) / previousDataViews) * 100;
 
-// Calculate percentage change
-const percentChange = (currentData - previousData) / previousData * 100;
-const percentChangeViews = (currentDataViews - previousDataViews) / previousDataViews * 100;
+    return { currentData, previousData, currentDataViews, previousDataViews, percentChange, percentChangeViews, realTimeVisitors };
+}
 
-// Add widget text
-const title = widget.addText("📈 Statistics");
-title.font = Font.boldSystemFont(14);
-title.textColor = new Color("#ffffff");
+// Configure widget
+const widget = new ListWidget();
+widget.backgroundColor = new Color("#000000");
+const widgetSize = config.widgetFamily;
 
-widget.addSpacer(4);
+(async () => {
+    // Retrieve data for the widget
+    const data = await fetchDataAndCalculate();
 
-// Use emojis for a visual representation
-const currentVisitorsText = `last 7 days: ${currentData} 👤`;
-const previousVisitorsText = `Previous: ${previousData} 👤`;
+    if (widgetSize === 'small') {
+        const title = widget.addText("📈 Statistics");
+        title.font = Font.boldSystemFont(14);
+        title.textColor = new Color("#ffffff");
 
-const currentViewsText = `last 7 days: ${currentDataViews} 👁️‍🗨️`;
-const previousViewsText = `Previous: ${previousDataViews} 👁️‍🗨️`;
+        widget.addSpacer(4);
 
-// Group visitor and page view counts
-const statsText = widget.addText(`${currentVisitorsText}\n${currentViewsText}\n\n${previousVisitorsText}\n${previousViewsText}`);
-statsText.font = Font.systemFont(12);
-statsText.textColor = new Color("#ffffff");
+        const statsText = widget.addText(`last 7 days: ${data.currentData} 👤\nPrevious: ${data.previousData} 👤`);
+        statsText.font = Font.systemFont(12);
+        statsText.textColor = new Color("#ffffff");
+        
+        widget.addSpacer(4);
+        
+        const viewsText = widget.addText(`last 7 days: ${data.currentDataViews} 👁️‍🗨️\nPrevious: ${data.previousDataViews} 👁️‍🗨️`);
+        viewsText.font = Font.systemFont(12);
+        viewsText.textColor = new Color("#ffffff");
 
-widget.addSpacer(4);
+        widget.addSpacer(4);
 
-const percentChangeText = `Visitors: ${percentChange >= 0 ? '+' : ''}${percentChange.toFixed(2)}%`;
-const percentChangeViewsText = `Views: ${percentChangeViews >= 0 ? '+' : ''}${percentChangeViews.toFixed(2)}%`;
+        const changeText = widget.addText(`👤 Visitors: ${data.percentChange.toFixed(2)}%`);
+        changeText.font = Font.systemFont(12);
+        changeText.textColor = data.percentChange >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+        
+        const changeTextViews = widget.addText(`👁️‍🗨️ Views: ${data.percentChangeViews.toFixed(2)}%`);
+        changeTextViews.font = Font.systemFont(12);
+        changeTextViews.textColor = data.percentChange >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+    } else if (widgetSize === 'medium') {
+        const horizontalStack = widget.addStack();
+        horizontalStack.layoutHorizontally();
 
-// Combine percentage changes in one line if space is limited
-const changeText = widget.addText(`${percentChangeText}`);
-changeText.font = Font.systemFont(12);
-changeText.textColor = percentChange >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+        const leftStack = horizontalStack.addStack();
+        leftStack.layoutVertically();
 
-const changeTextView = widget.addText(`${percentChangeViewsText}`);
-changeTextView.font = Font.systemFont(12);
-changeTextView.textColor = percentChangeViews >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+        const titleMedium = leftStack.addText("📈 Statistics");
+        titleMedium.font = Font.boldSystemFont(14);
+        titleMedium.textColor = new Color("#ffffff");
 
-// Display widget
-Script.setWidget(widget);
-Script.complete();
+        leftStack.addSpacer(4);
 
-widget.presentSmall();
+        const statsText = leftStack.addText(`last 7 days: ${data.currentData} 👤\nPrevious: ${data.previousData} 👤`);
+        statsText.font = Font.systemFont(12);
+        statsText.textColor = new Color("#ffffff");
+            
+        leftStack.addSpacer(4);
+            
+        const viewsText = leftStack.addText(`last 7 days: ${data.currentDataViews} 👁️‍🗨️\nPrevious: ${data.previousDataViews} 👁️‍🗨️`);
+        viewsText.font = Font.systemFont(12);
+        viewsText.textColor = new Color("#ffffff");
+
+        leftStack.addSpacer(4);
+
+        const changeTextMedium = leftStack.addText(`👤 Visitors: ${data.percentChange.toFixed(2)}%`);
+        changeTextMedium.font = Font.systemFont(12);
+        changeTextMedium.textColor = data.percentChange >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+        
+        const changeTextViewsMedium = leftStack.addText(`👁️‍🗨️ Views: ${data.percentChangeViews.toFixed(2)}%`);
+        changeTextViewsMedium.font = Font.systemFont(12);
+        changeTextViewsMedium.textColor = data.percentChange >= 0 ? new Color("#00fa9a") : new Color("#ff4500");
+
+        horizontalStack.addSpacer();
+        const rightStack = horizontalStack.addStack();
+        rightStack.layoutVertically();
+        rightStack.size = new Size(0, 0);
+
+        const placeholder = rightStack.addText("👤 Realtime Visitors");
+        placeholder.font = Font.boldSystemFont(14);
+        placeholder.textColor = new Color("#ffffff");
+        
+        rightStack.addSpacer(4);
+        
+        // Add real-time visitor data to the widget
+        if (data.realTimeVisitors && data.realTimeVisitors.length > 0) {
+            data.realTimeVisitors.forEach(visitor => {
+                const visitorText = `${visitor.lastActionDateTime.split(' ')[1].substring(0, 5)} - ${visitor.country} - ${visitor.actionDetails.length} 👁️‍🗨️`;
+                const textElement = rightStack.addText(visitorText);
+                textElement.font = Font.systemFont(10);
+                textElement.textColor = new Color("#ffffff");
+            });
+        } else {
+            // Placeholder text in case there is no real-time visitor data
+            const placeholder = rightStack.addText("No current visitor data available.");
+            placeholder.font = Font.systemFont(12);
+            placeholder.textColor = new Color("#ffffff");
+        }
+        
+        //rightStack.addSpacer();
+    }
+
+    // Show Widget
+    Script.setWidget(widget);
+    Script.complete();
+
+    if (widgetSize === 'small') {
+        widget.presentSmall();
+    } else if (widgetSize === 'medium') {
+        widget.presentMedium();
+    }
+})();
